ad0fb03f03b092273a154861be8ee1b444edd540
admin <admin@YD-04>
2022-09-05 18:03:45 +0900

    20220905

--- helloWorld2/src/main/java/jdbc/MemberManage.java
+++ helloWorld2/src/main/java/jdbc/MemberManage.java
@@ -17,6 +17,76 @@ public class MemberManage extends DAO{
 		return mm;
 	}
 	
+	// 풀캘린더 관련(from).
+	public List<FullCalendar> scheduleList() {
+		List<FullCalendar> list = new ArrayList<>();
+		String sql = "select * from my_calendar";
+		
+		conn();
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				FullCalendar cal = new FullCalendar();
+				cal.setTitle(rs.getString("title"));
+				cal.setStartDate(rs.getString("start_date"));
+				cal.setEndDate(rs.getString("end_date"));
+				list.add(cal);
+				
+			}
+					
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			disconnect();
+		}
+		return list;
+	}
+	// 한건 입력.
+	public boolean insertCalendar(FullCalendar full) {
+		String sql = "insert into my_calendar values(?,?,?)";
+		conn();
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, full.getTitle());
+			pstmt.setString(2, full.getStartDate());
+			pstmt.setString(3, full.getEndDate());
+			int r = pstmt.executeUpdate();
+			if(r>0)
+				return true;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return false;
+	}
+	
+	// 삭제.
+	public boolean deleteCalendar(FullCalendar full) {
+		String sql = "delete from my_calendar where title = ?";
+		conn();
+		try {
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,full.getTitle());
+			
+			int r = pstmt.executeUpdate();
+			if(r>0)
+				return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return false;
+	}
+	
+	
+	
+	// 풀캘린더 관련(to).
+	
 	//로그인 메소드
 	public Member loginInfo(String id) {
 		Member member = null;
--- helloWorld2/src/main/webapp/javascript/dom_manipulate2.js
+++ helloWorld2/src/main/webapp/javascript/dom_manipulate2.js
@@ -1,189 +1,195 @@
 // dom_manipulate2.js // 학습용
+// 자바스크립트의 객체는 키(key)과 값(value)으로 구성된 프로퍼티(Property)들의 집합.
+
 const members = [
-    {mid: 'admin', mname: '관리자', mphone: '010-1111-2222'},
-    {mid: 'hong12', mname: '홍길동', mphone: '010-2345-8765'},
-    {mid: 'parkgr', mname: '박규림', mphone: '010-9873-2312'}
-
-
+    {mid: 'admin', mname: '관리자', mphone: '010-1111-1122'},
+    {mid: 'hong12', mname: '홍길동', mphone: '010-6633-9872'},
+    {mid: 'parkgr', mname: '박규림', mphone: '010-3355-1462'}
 ]
 
+document.addEventListener('DOMContentLoaded',function() {
+    // addEventListener:dom 요소에 이벤트 등록하는 메서드. (이벤트 타입, 이벤트 기능)
+    // DOMContentLoaded 가 다 다운받아진 후에 function 함수 실행.
 
-document.addEventListener('DOMContentLoaded', function () {
     showMember();
- 
-    // 등록버튼 이벤트.
-    let addBtn = document.querySelector('button');
+    // 회원등록 버튼 이벤트.
+    // addBtn.onclick ='click' 속성넣기. <button>에 적용될 메서드 만들어짐.
+    // querySelector: html의 클래스 이름 및 태그를 갖고 오기 위해서 사용.
+    let addBtn = document.querySelector('button')
     addBtn.addEventListener('click', addMember);
-    // 회원찾기 이벤트.
-    let findBtn = document.getElementById('findMember');
+    
+    
+    // 회원찾기 버튼 이벤트.
+    let findBtn = document.getElementById('findMember')
     findBtn.addEventListener('click', findMember);
-    // 전체선택/해제 이벤트.
-    let allCheck = document.getElementById('allCheck');
-    allCheck.addEventListener('change', allChecked);
     
-}); // dom요소에 이벤트 등록.
-function allChecked() {
-    // true/ false 값은 this의 checked 값을 할당.
-    let chks = document.querySelectorAll('tbody input[type="checkbox"]');
-    chks.forEach(check => {
-        check.checked = this.checked; // prop
-    })
-}
+    // 전체 선택 or 해제 이벤트.
+    let allcheck = document.getElementById('allcheck')
+    allcheck.addEventListener('change', allchecked);
 
-function findMember() {
-    // 찾을 아이디.
-    let id =  document.getElementById('mid').value; // getElementById('mid'):input 태그  .value:admin
-    console.log(id);
-    
-    let trs = document.getElementsByTagName('tbody')[0].children;
-    trs = document.querySelectorAll('tbody>tr'); // tbody>tr : 모두 출력     :nth-chile(1) 순서도 가능
-    console.log(trs); // NodeList [tr, tr, tr]
-    trs.forEach(tr => {
-        // tr.className = ''; // 클래스 속성값을 변경. 클래스 속성이 남아있음
-        tr.removeAttribute('class'); // 위의 기능과 같음.
-    })
-    trs.forEach(tr => {
-        console.log(tr.firstElementChild.textContent);
-        if (tr.firstElementChild.textContent == id) {
-            tr.setAttribute('class', 'focus');
-            // 값 변경.
-            let childTd = tr.children;
-            childTd[1].textContent = document.getElementById('mname').value;
-            childTd[2].textContent = document.getElementById('mphone').value;
-        }
+}) ; // dom 요소에 이벤트 등록.
 
-    });
-}
+
+
 
 function showMember() {
     // 리스트 보여주기.
-    let table = document.getElementById('list');     // getElementById
+    let table = document.getElementById('list');
+
     members.forEach(member => {
-        let mValues = [];
-        // // object -> array.
+        let mValues = []; // object -> array []
+        // Object.values: 파라메타로 전달된 객체의  value값만 뽑아서 배열로 반환.
+        // for in 구문 결과와 동일.
         // for (let prop in member) {
         //     mValues.push(member[prop]);
         // }
-        // let tr = makeTr(mValues);
-        mValues = Object.values(member); // Object.keys(member) : [속성,속성]
+        mValues = Object.values(member);
         let tr = makeTr(mValues);
-        // let tr = document.createElement('tr'); // <tr></tr> 여기부터
-        // for(let prop in member) {
-        //     let td = document.createElement('td'); // <td></td>
-        //     let txt = document.createTextNode(member[prop]); // admin 관리자 연락처
-        //     td.appendChild(txt);
-        //     tr.appendChild(td);
-        // }
-        // // 삭제버튼 추가.
-        // let td = document.createElement('td');
-        // let btn = document.createElement('button'); // <button>삭제</button>
-        // let txt = document.createTextNode('삭제') ;
-        
-        // btn.appendChild(txt);
-        // tr.appendChild(btn);
-        // tr.appendChild(td); // 여기까지 tr 만들어주는 코드
-        table.appendChild(tr); // append appendChild 둘다 사용 가능.
-
-
+        table.appendChild(tr);
     });
     
-}
 
-function addMember() {    
+function addMember() { 
+    // getElementById: 주어진 문자열과 일치하는 id 속성을 가진 요소를 찾고, 
+    //이를 나타내는 Element 객체를 반환
+    // mid 와 일치하는 id를 찾아서 그 id가 가진 속성을 반환해라.
     let mid = document.getElementById('mid');
     let mname = document.getElementById('mname');
     let mphone = document.getElementById('mphone');
     console.log(mid.value, mname.value, mphone.value);
-
-
-
+    
     // 저장값이 없으면 warning.
-    if(!mid.value || !mname.value || !mphone.value) {
-        alert ('필수값을 입력하세요!!!');
-        return; // 함수종료.
+    if (!mid.value || !mname.value || !mphone.value) {
+        alert('필수값을 입력하세요!!!');
+        return; //  break 역할.함수 실행을 종료하고, 주어진 값을 함수 호출 지점으로 반환.
     }
 
-    const mValues = [mid.value, mname.value, mphone.value]
+    const mValues = [mid.value, mname.value, mphone.value];
     let tr = makeTr(mValues);
-    
-
-    // let tr = document.createElement('tr');
-
-    // mValues.forEach(val => {
-    //     let td = document.createElement('td'); // id위치 <td>user1</td>
-    //     let txt = document.createTextNode(val);
-    //     td.appendChild(txt); // <td>user1</td>
-    //     tr.appendChild(td); // <tr><td>user1</td></tr>
-    // });
-    // // 회원삭제버튼 추가. 
-    // let td = document.createElement('td');
-    //     let btn = document.createElement('button'); // <button>삭제</button>
-    //     let txt = document.createTextNode('삭제') ;
-        
-    //     btn.appendChild(txt);
-    //     tr.appendChild(btn);
-    //     tr.appendChild(td);
-    document.getElementById('list').appendChild(tr);
-
+    document.getElementById('list').appendChild(tr)
+   
     // 초기화.
     mid.value = '';
     mname.value = '';
     mphone.value = '';
     mid.focus();
-} // end of addMember().
 
-function makeTr(mValues=[]) { //버튼 만드는 부분
+} // end of addMember;
 
+function makeTr(mValues = []) {
+    // createElement: 지정한 tagName의 HTML 요소를 만들어 반환
     let tr = document.createElement('tr');
-    tr.addEventListener('click', function() {
-        console.log(this.firstElementChild.innerText); //<td><button>user1</button></td>
+    tr.addEventListener('click', function () {
         // 선택된 tr의 정보를 입력화면에 보여지도록.
-        document.getElementById('mid').value = this.firstElementChild.innerText; // id
-        document.getElementById('mname').value = this.firstElementChild.innerText; // name
-        document.getElementById('mphone').value = this.firstElementChild.innerText; // mphone
-        
-    }, true); // 상위부터 이벤트 찾아서 전파?
+        document.getElementById('mid').value = this.firstElementChild.innerText;
+        document.getElementById('mname').value = this.firstElementChild.nextElementSibling.innerText;
+        document.getElementById('mphone').value = this.firstElementChild.nextElementSibling.nextElementSibling.innerText;
+}, false); // true : 캡처링  false : 버블링 (=default 값. 생략가능)
 
-    // 체크박스 추가.
-    // <tr><td><input type='checkbox'></td></tr>
-    let chk = document.createElement('input');
-    chk.addEventListener('click', function(e) {
-        // tr의 이벤트가 발생하지 않도록 하기 위해서.
-        e.stopPropagation();
-    })
-    chk.addEventListener('change', function(){
-        let checkProp = document.querySelectorAll('tbody tr input[type="checkbox"]');
     
-        //[input, input, input] 배열(forEach) : every(), some()
-        document.getElementById('allCheck').checked = 
-                                     [...checkProp].every(item => {return item.checked == true});
+//td생성
+mValues.forEach(val => {
+    let td = document.createElement('td'); // <td>user1</td>
+    let txt = document.createTextNode(val);
+    td.appendChild(txt); //<td>user1</td> => td가 감싸고 있으므로 자식요소로 봄
+    tr.appendChild(td); //<tr><td>user1</td></tr>
+});
 
-    })
-    // chk.checked = true; // 체크.    
-    chk.setAttribute('type','checkbox');
-    td = document.createElement('td');
-    td.appendChild(chk);
-    td.appendChild(td);
+// 삭제버튼 추가만.
+let td = document.createElement('td');
+let btn = document.createElement('button');
+let txt = document.createTextNode('삭제');
 
-    mValues.forEach(val => {
-        let td = document.createElement('td'); // id위치 <td>user1</td>
-        let txt = document.createTextNode(val);
-        td.appendChild(txt); // <td>user1</td>
-        tr.appendChild(td); // <tr><td>user1</td></tr>
+// 삭제버튼에 삭제 기능 추가.
+btn.addEventListener('click', function (e) {
+     //btn의 부모인 <td></td> => td의 부모인 <tr></tr> 안의 내용을 삭제.
+     this.parentElement.parentElement.remove();
+     e.stopPropagation(); // 이벤트 전파 차단.
+
+}, false);
+
+btn.appendChild(txt);
+td.appendChild(btn);
+tr.appendChild(td);
+
+// <tr><td><button>삭제</button> </td></tr>
+
+    
+        
+// 체크박스 추가.
+//<tr><td><input id="allcheck" type="checkbox"></td></tr>
+let chk = document.createElement('input');
+chk.addEventListener('click', function (e) {
+    // 자식 input 이벤트 발생시 같은 이벤트 설정되어있는 부모로의 이벤트 전파 방지.
+    e.stopPropagation();
+
+});
+
+chk.addEventListener('change', function () {
+    // mid, mname, mphone 는 속성(property).
+    // checkProp: 
+    // querySelector: 선택자를 사용하여 요소 찾기. 같은 id 또는 class 일 경우 스크립트의 최상단 요소만 포함.
+    // querySelectorAll: 선택자를 선택하여 배열과 비슷한 객체인 nodeList를 반환. 반환객체가 nodeList이기에 for문 또는 forEach문을 사용.
+    //[input, input, input => 총 3개] 배열(forEach) : every(), some()
+    let checkProp = document.querySelectorAll('tbody input[type="checkbox"]');
+    // let checkAry =[];
+    // checkProp.forEach(item=>{
+    //     checkAry.push*(item);
+    // })
+    // [input, input, input]배열 (forEach) : every(), some()
+     // 요소를 한번에 가져올수있음.
+    // every나 some:  배열내 값이 존재하는지 확인할때나 조건에 맞는(혹은 맞지 않는) 값이 있는지 배열 내부 순회하면서 검사.
+    document.getElementById('allcheck').checked = [...checkProp].every(item => item.checked);
+        // ... : 나머지 파라메타 (배열 처럼 만들어짐)
+
+})
+
+// chk.checked = true; //체크.
+chk.setAttribute('type', 'checkbox');  
+td = document.createElement('td');
+td.appendChild(chk);
+tr.appendChild(td);
+
+
+
+
+return tr;
+
+}
+        
+                     
+function findMember() {
+    // 찾을 아이디.
+    let id = document.getElementById('mid').value;
+    console.log(id);
+
+    let trs = document.getElementsByTagName('tbody')[0].children;
+    trs = document.querySelectorAll('tbody>tr');
+    console.log(trs); // NodeList[tr,tr,tr]
+
+    trs.forEach(tr => {
+         //tr.className = ''; // 클래스 속성값을 변경.
+         tr.removeAttribute('class'); // 클래스 속성을 지워줌 .
+     })
+
+     trs.forEach(tr => {
+        console.log(tr.firstElementChild.textContent); //textContent 또는 innerText 써주기.
+        if (tr.firstElementChild.textContent == id) {
+            tr.setAttribute('class', 'focus');
+            //값 변경
+            let childTd = tr.children
+            childTd[1].textContent = document.getElementById('mname').value;
+        childTd[2].textContent = document.getElementById('mphone').value;
+
+    
+        }
     });
-    // 회원삭제버튼 추가. 
-    let td = document.createElement('td');
-        let btn = document.createElement('button'); // <button>삭제</button>
-        let txt = document.createTextNode('삭제') ;
-        btn.addEventListener('click', function(e) { // 이벤트 달아보자.
-            this.parentElement.parentElement.remove();  // tr 삭제.
-            e.stopPropagation(); // 하나 실행하고 이벤트 전파 차단. 상위에 존재하는 이벤트 실행 안되게함.
-        }, false); // 기본값 false.
-       
-        btn.appendChild(txt);
-        td.appendChild(btn);
-        tr.appendChild(td);
+    }
 
-    return tr;
-
+function allchecked() {
+     // true/ false 값은 this의 checked 값을 할당
+     let chks = document.querySelectorAll('tbody input[type="checkbox"]');
+     chks.forEach(check => {
+        check.checked = this.checked; //prop, this = allcheck
+  })
 }
